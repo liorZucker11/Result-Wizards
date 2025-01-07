3eb3dd63e2a79b77d2c33d89329da6b74e918979
yoni4600 <yoni4600@gmail.com>
2025-01-07 21:27:52 +0200

    fix

--- scrap-project/app.py
+++ scrap-project/app.py
@@ -27,7 +27,7 @@ chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36")
 chrome_options.binary_location = "/opt/render/project/.render/chrome/opt/google/chrome/google-chrome"  # Path to Chrome binary
 
-service = Service(ChromeDriverManager(chrome_type=ChromeType.GOOGLE, version="latest").install())
+service = Service(ChromeDriverManager(chrome_type=ChromeType.GOOGLE).install())
 driver = webdriver.Chrome(service=service, options=chrome_options)
 
 page_texts = []
