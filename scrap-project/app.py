b27593a4da5eff01e71de03144f57a8e6ec695c5
yoni4600 <yoni4600@gmail.com>
2025-01-17 19:23:15 +0200

    project QA

--- scrap-project/app.py
+++ scrap-project/app.py
@@ -401,6 +401,11 @@ def hw3():
     return render_template("hw3.html")
 
 
+@app.route("/qa")
+def QA():
+    return render_template("QA.html")
+
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=True, use_reloader=False)
